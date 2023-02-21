f307b10f28bb9421395e975c025e83b5eb06981c
HarithKavish <harithkavish40@gmail.com>
2023-02-22 00:39:42 +0530

    commit

---
+++ src/main/java/com/example/demo/D2P1ValueAnnotationApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class D2P1ValueAnnotationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(D2P1ValueAnnotationApplication.class, args);
+	}
+
+}
